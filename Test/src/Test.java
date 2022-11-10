bcd86c8c30e557eea2e5c395e31ba65390ddf8ed
aymenh023 <117355462+aymenh023@users.noreply.github.com>
2022-11-10 13:28:09 +0100

    Update Test.java
    Optimisation du code et ajout de prise en compte du solde de l'entreprise

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,76 +1,137 @@
 import java.time.LocalDate;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Test {
 
-	private static HashMap<String, Gift> gifts = new HashMap<>();
-	private static HashMap<String, Meal> meals = new HashMap<>();
+	private static Map<String, Gift> gifts = new LinkedHashMap<>();
+	private static Map<String, Meal> meals = new LinkedHashMap<>();
 
-	private static Integer balance = 0;
+	private static Integer companyBalance = 500;
+	private static Integer userBalance = 0;
 
 	public static void main(String[] args) {
-		distibuteGift("gift A", 150);
-		distibuteGift("gift B", 100);
-		distibuteMeal("meal A", 20);
-		distibuteMeal("meal B", 10);
-		calculateBalance();
+		distibuteGift("gift A", LocalDate.now(), 150);
+		distibuteGift("gift B", LocalDate.now(), 100);
+		distibuteGift("gift C", LocalDate.now(), 400);
+		distibuteMeal("meal A", LocalDate.now(), 20);
+		distibuteMeal("meal B", LocalDate.now(), 10);
+		distibuteMeal("meal C", LocalDate.now(), 250);
+		calculateUserBalance();
 	}
 
-	public static void distibuteGift(String giftName, Integer giftValue) {
-		LocalDate dateNow = LocalDate.now();
-		gifts.put(giftName, new Gift(dateNow, giftValue));
+	/**
+	 * Méthode pour distribuer des Gifts
+	 * 
+	 * @param giftName
+	 * @param crationDate
+	 * @param giftValue
+	 */
+	public static void distibuteGift(String giftName, LocalDate crationDate, Integer giftValue) {
+		Gift gift = new Gift(giftName, crationDate, giftValue);
+		gifts.put(gift.getName(), gift);
 	}
 
-	public static void distibuteMeal(String mealName, Integer mealValue) {
-		LocalDate dateNow = LocalDate.now();
-		meals.put(mealName, new Meal(dateNow, mealValue));
+	/**
+	 * Méthode pour distribuer des Meals
+	 * 
+	 * @param mealName
+	 * @param crationDate
+	 * @param mealValue
+	 */
+	public static void distibuteMeal(String mealName, LocalDate crationDate, Integer mealValue) {
+		Meal meal = new Meal(mealName, crationDate, mealValue);
+		meals.put(meal.getName(), meal);
 	}
 
-	public static Integer calculateBalance() {
+	/**
+	 * Méthode pour : </br>
+	 * incrémenter le solde du bénéficiaire si seulmenet le solde de l'entreprise est positif </br>
+	 * décrémenter le solde de l'entreprise si seulement il reste positif
+	 * 
+	 * @param value
+	 * @return true, si les soldes ont été traités correctement
+	 */
+	public static boolean incrementUserBalance(Integer value) {
+		if (companyBalance - value >= 0) {
+			companyBalance -= value;
+			userBalance += value;
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Méthode pour calculer le solde du bénéficiaire
+	 */
+	public static void calculateUserBalance() {
 		LocalDate dateNow = LocalDate.now();
 
 		System.out.println("Today : " + dateNow);
 
-		for (Map.Entry<String, Gift> gift : gifts.entrySet()) {
-			LocalDate giftExpirationDate = gift.getValue().getCreationDate().plusYears(1).minusDays(1);
+		LocalDate giftExpirationDate = null;
+		LocalDate mealExpirationDate = null;
+		Integer giftValue = null;
+		Integer mealValue = null;
+		Gift gift = null;
+		Meal meal = null;
+
+		for (Map.Entry<String, Gift> giftEntry : gifts.entrySet()) {
+			gift = giftEntry.getValue();
+			giftExpirationDate = gift.getCreationDate().plusYears(1).minusDays(1);
+			gift.setExpirationDate(giftExpirationDate);
 
 			if (dateNow.isBefore(giftExpirationDate)) {
-				Integer giftValue = gift.getValue().getValue();
-				System.out.println("Gift Value : " + giftValue + "£ / Gift Expiration Date : " + giftExpirationDate);
-				balance += giftValue;
+				giftValue = gift.getValue();
+				if (incrementUserBalance(giftValue)) {
+					System.out.println(gift.toString());
+				}
 			}
 		}
 
-		for (Map.Entry<String, Meal> meal : meals.entrySet()) {
-			LocalDate mealExpirationDate = LocalDate.of(meal.getValue().getCreationDate().getYear() + 1, 2, 1);
+		for (Map.Entry<String, Meal> mealEntry : meals.entrySet()) {
+			meal = mealEntry.getValue();
+			mealExpirationDate = LocalDate.of(meal.getCreationDate().getYear() + 1, 2, 1);
 			mealExpirationDate = mealExpirationDate.withDayOfMonth(mealExpirationDate.getMonth().length(mealExpirationDate.isLeapYear()));
+			meal.setExpirationDate(mealExpirationDate);
 
 			if (dateNow.isBefore(mealExpirationDate)) {
-				Integer mealValue = meal.getValue().getValue();
-				System.out.println("Meal Value : " + mealValue + "£ / Meal Expiration Date : " + mealExpirationDate);
-				balance += mealValue;
+				mealValue = meal.getValue();
+				if (incrementUserBalance(mealValue)) {
+					System.out.println(meal.toString());
+				}
 			}
 		}
 
-		System.out.println("User balance is : " + balance);
-
-		return balance;
+		System.out.println("User balance is : " + userBalance + "£");
+		System.out.println("Company balance is : " + companyBalance + "£");
 	}
 
 	private static class Deposit {
+		protected String name;
 		protected LocalDate creationDate;
 		protected Integer value;
+		protected LocalDate expirationDate;
 
 		/**
 		 * Constructeur public avec paramètres
 		 * 
+		 * @param name
 		 * @param creationDate
 		 * @param value
 		 */
-		public Deposit(LocalDate creationDate, Integer value) {
+		public Deposit(String name, LocalDate creationDate, Integer value) {
+			this.name = name;
 			this.creationDate = creationDate;
 			this.value = value;
+		}
+
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
 		}
 
 		/**
@@ -86,17 +147,39 @@ public class Test {
 		public Integer getValue() {
 			return value;
 		}
+
+		/**
+		 * @param expirationDate the expirationDate to set
+		 */
+		public void setExpirationDate(LocalDate expirationDate) {
+			this.expirationDate = expirationDate;
+		}
+
 	}
 
 	private static class Gift extends Deposit {
 		/**
 		 * Constructeur public avec paramètres
 		 * 
+		 * @param name
 		 * @param creationDate
 		 * @param value
 		 */
-		public Gift(LocalDate creationDate, Integer value) {
-			super(creationDate, value);
+		public Gift(String name, LocalDate creationDate, Integer value) {
+			super(name, creationDate, value);
+		}
+
+		@Override
+		public String toString() {
+			return "Gift Added To User Balance [name="
+				+ name
+				+ ", creationDate="
+				+ creationDate
+				+ ", value="
+				+ value
+				+ ", expirationDate="
+				+ expirationDate
+				+ "]";
 		}
 	}
 
@@ -104,11 +187,25 @@ public class Test {
 		/**
 		 * Constructeur public avec paramètres
 		 * 
+		 * @param name
 		 * @param creationDate
 		 * @param value
 		 */
-		public Meal(LocalDate creationDate, Integer value) {
-			super(creationDate, value);
+		public Meal(String name, LocalDate creationDate, Integer value) {
+			super(name, creationDate, value);
+		}
+
+		@Override
+		public String toString() {
+			return "Meal Added To User Balance [name="
+				+ name
+				+ ", creationDate="
+				+ creationDate
+				+ ", value="
+				+ value
+				+ ", expirationDate="
+				+ expirationDate
+				+ "]";
 		}
 	}
 
